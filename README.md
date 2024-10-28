ac88863298a48fd2108d000755b7f23207bf44cf
Sachin Acharya <51048248+sachin-acharya-projects@users.noreply.github.com>
2024-10-28 21:39:09 +0545

    formated codes

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # flask-youtube-api
+
 Flask YouTube api
-_____________________________________________________________________________
+
+---
+
 ## API URL(S)
-1. **Current**  
+
+1. **Current**
+
     [http://sachinacharya.pythonanywhere.com/](http://sachinacharya.pythonanywhere.com/)
--- app.py
+++ app.py
@@ -1,86 +1,107 @@
 from crypt import methods
-from youtubesearchpython import VideosSearch, Playlist, Video, StreamURLFetcher, ResultMode, CustomSearch, SearchMode
+from youtubesearchpython import (
+    VideosSearch,
+    Playlist,
+    Video,
+    StreamURLFetcher,
+    ResultMode,
+    CustomSearch,
+    SearchMode,
+)
 from flask import request, jsonify, Flask
 import os, json, dotenv
 
-# Loading Defaults
 dotenv.load_dotenv()
 app = Flask(__name__)
 SECRET_KEY = os.getenv("SECRET_KEY")
 fetcher = StreamURLFetcher()
 
+
 def save(data: VideosSearch):
-    with open('data.json', 'w') as file:
+    with open("data.json", "w") as file:
         file.seek(0)
         file.truncate()
         json.dump(data, file, indent=4)
 
-# root: Test
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def root():
-    return jsonify({
-        "_": "Format for requesting data. (ommit 'message' parameter) (require JSON)",
-        "secret_key": "SECRET_REGISTERED_KEY",
-        "media_url": "YouTube url of Playlist or Video",
-        "video_title": "Title of YouTube video",
-        "limit": "<int: 5>",
-        "endpoints": [
-            "/api/video",
-            "/api/playlist",
-            "/api/search",
-        ]
-    })
-# POST: Search YouTube Video
-@app.route("/api/search/", methods=['POST'])
+    return jsonify(
+        {
+            "_": "Format for requesting data. (ommit 'message' parameter) (require JSON)",
+            "secret_key": "SECRET_REGISTERED_KEY",
+            "media_url": "YouTube url of Playlist or Video",
+            "video_title": "Title of YouTube video",
+            "limit": "<int: 5>",
+            "endpoints": [
+                "/api/video",
+                "/api/playlist",
+                "/api/search",
+            ],
+        }
+    )
+
+
+@app.route("/api/search/", methods=["POST"])
 def search_video():
     queries = request.args
-    secret_key = queries.get('secret_key', None)
+    secret_key = queries.get("secret_key", None)
     if secret_key and secret_key == SECRET_KEY:
         try:
-            video_name = queries['video_title']
+            video_name = queries["video_title"]
         except:
-            return jsonify({
-                "message": "Invalid input parameter ── video_title",
-                "status": "failed",
-                "status_code": 400
-            }), 400
-        limit = int(queries.get('limit', "5"))
+            return (
+                jsonify(
+                    {
+                        "message": "Invalid input parameter ── video_title",
+                        "status": "failed",
+                        "status_code": 400,
+                    }
+                ),
+                400,
+            )
+        limit = int(queries.get("limit", "5"))
         if limit > 20:
             limit = 20
         searched_videos = VideosSearch(video_name, limit)
         return searched_videos.result()
     else:
-        return jsonify({
-            "message": "unauthorized user",
-            "help": "recheck secret_key for invalidity or incorrectness",
-            "status": "failed",
-            "accepted_queries": [
-                "secret_key (required) ── Secret Authorization Key",
-                "video_title (required) ── Name or title of YouTube video to search",
-                "limit (optional, default: 5) ── Total number of results required, max. 20"
-            ],
-            "request_time": "unlimited",
-            "status_code": 401
-        }), 401
+        return (
+            jsonify(
+                {
+                    "message": "unauthorized user",
+                    "help": "recheck secret_key for invalidity or incorrectness",
+                    "status": "failed",
+                    "accepted_queries": [
+                        "secret_key (required) ── Secret Authorization Key",
+                        "video_title (required) ── Name or title of YouTube video to search",
+                        "limit (optional, default: 5) ── Total number of results required, max. 20",
+                    ],
+                    "request_time": "unlimited",
+                    "status_code": 401,
+                }
+            ),
+            401,
+        )
 
-# POST: Scrape YouTube Playlist
-@app.route("/api/playlist/", methods=['POST'])
+
+@app.route("/api/playlist/", methods=["POST"])
 def get_playlist():
     pass
 
-# POST: Scrape YouTube Video
-@app.route("/api/video/", methods=['POST'])
+
+@app.route("/api/video/", methods=["POST"])
 def get_videos():
     queries = request.args
-    secret_key = queries.get('secret_key', None)
+    secret_key = queries.get("secret_key", None)
     if secret_key and secret_key == SECRET_KEY:
         try:
-            media_url = queries['media_url']
-            result_mode = queries['result_mode']
-            videos = ''
-            if result_mode == 'info':
+            media_url = queries["media_url"]
+            result_mode = queries["result_mode"]
+            videos = ""
+            if result_mode == "info":
                 videos = Video.getInfo(media_url, mode=ResultMode.json)
-            elif result_mode == 'formats':
+            elif result_mode == "formats":
                 stuff = Video.get(media_url, mode=ResultMode.json)
                 videos = fetcher.getAll(stuff)
             else:
@@ -88,23 +109,35 @@ def get_videos():
             return videos
         except Exception as e:
             print(e)
-            return jsonify({
-                "message": "invalid input parameters",
-                "status": "failed",
-                "status_code": 400
-            }), 400
-        
+            return (
+                jsonify(
+                    {
+                        "message": "invalid input parameters",
+                        "status": "failed",
+                        "status_code": 400,
+                    }
+                ),
+                400,
+            )
+
     else:
-        return jsonify({
-            "message": "unauthorized user",
-            "help": "recheck secret_key for invalidity or incorrectness",
-            "status": "failed",
-            "accepted_queries": [
-                "media_url (required) ── YouTube video URL (/watch?v=)",
-                "result_mode (required) ── accepted_value: [all | formats | info]"
-            ],
-            "request_time": "unlimited",
-            "status_code": 401
-        }), 401
-if __name__ == '__main__':
+        return (
+            jsonify(
+                {
+                    "message": "unauthorized user",
+                    "help": "recheck secret_key for invalidity or incorrectness",
+                    "status": "failed",
+                    "accepted_queries": [
+                        "media_url (required) ── YouTube video URL (/watch?v=)",
+                        "result_mode (required) ── accepted_value: [all | formats | info]",
+                    ],
+                    "request_time": "unlimited",
+                    "status_code": 401,
+                }
+            ),
+            401,
+        )
+
+
+if __name__ == "__main__":
     app.run(debug=True)
